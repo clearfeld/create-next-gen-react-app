4c96118b1035af40b1fc3e4d434619d33184ef5b
Marko R <clearfeld@outlook.com>
2023-01-25 12:22:53 -0500

    Add: Docs: how to add sass

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>Create Next Gen React App</span>,
-  project: {
-    link: 'https://github.com/clearfeld/create-next-gen-react-app',
-  },
-  editLink: {
-    text: "Edit this page on GitHub",
-  },
-  docsRepositoryBase: "https://github.com/clearfeld/create-next-gen-react-app/tree/main/docs",
-  // chat: {
-  //   link: 'https://discord.com',
-  // },
-  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Create Next Gen React App',
-  },
-}
+	logo: <span>Create Next Gen React App</span>,
+	project: {
+		link: "https://github.com/clearfeld/create-next-gen-react-app",
+	},
+	editLink: {
+		text: "Edit this page on GitHub",
+	},
+	docsRepositoryBase: "https://github.com/clearfeld/create-next-gen-react-app/tree/main/docs",
+	// chat: {
+	//   link: 'https://discord.com',
+	// },
+	// docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+	primaryHue: 29,
+	footer: {
+		text: "Create Next Gen React App",
+	},
+	useNextSeoProps() {
+		const { route } = useRouter();
+		if (route !== "/") {
+			return {
+				titleTemplate: "%s – CNGRA",
+			};
+		}
+	},
+};
 
-export default config
+export default config;
