ab84f0d2b84711b8d74a967e1160cc8ad0723c27
Marko R <clearfeld@outlook.com>
2023-01-25 00:26:25 -0500

    Add: init nextra docs

---
+++ docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Create Next Gen React App</span>,
+  project: {
+    link: 'https://github.com/clearfeld/create-next-gen-react-app',
+  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Create Next Gen React App',
+  },
+}
+
+export default config
