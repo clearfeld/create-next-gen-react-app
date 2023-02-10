492927161c46ddf12d70924bfc3027e442b9ecd4
Marko R <clearfeld@outlook.com>
2023-02-09 22:46:44 -0500

    Upgrade: stylelint to v15 across all templates

--- packages/create-next-gen-react-app-template-webpack/stylelint.config.cjs
+++ packages/create-next-gen-react-app-template-webpack/stylelint.config.cjs
@@ -2,9 +2,6 @@ module.exports = {
 	extends: "stylelint-config-standard",
 
 	rules: {
-		// consistency with Rome formatter default template settings
-		indentation: "tab",
-
 		// allow empty lines between css rules
 		"declaration-empty-line-before": null,
 	},
